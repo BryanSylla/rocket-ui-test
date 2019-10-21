2acf998503cb53c7ba1ee304ffe61ef04febc3b4
MARTHA M. SYLLA <Martha.Sylla@ibm.com>
2019-10-21 09:28:06 -0400

    added gitignore and url for rocket data from API

--- src/services/LaunchService.js
+++ src/services/LaunchService.js
@@ -2,11 +2,17 @@ import axios from 'axios';
 
 const SERVICES_URL = 'https://api.spacexdata.com/v3';
 const serviceUrl = `${SERVICES_URL}/launches`;
+const rocketServiceUrl = `${SERVICES_URL}/rockets`
 
 const api = axios.create();
 
-const launchService = {
-  get: () => api.get(`${serviceUrl}`)
-};
+const API_Services = {
+  launchService = {
+    get: () => api.get(`${serviceUrl}`)
+  },
+  rocketService = {
+    get: (rocket_id) => rocket_id ? api.get(`${rocketServiceUrl}/rocket_id`) : api.get(`${rocketServiceUrl}`)
+  }
+}
 
-export default launchService;
+export default API_Services;
